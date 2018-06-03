3bcf47afc119249cadd6d13215d76a27431168a9
KN21 <kr.newsome21@yahoo.com>
2018-06-03 14:34:56 -0400

    Added Error messages and cleanup

--- scraper.js
+++ scraper.js
@@ -1,13 +1,20 @@
 'use strict'
 
+//require packages
 const scrapeIt = require('scrape-it');
 const fs = require('fs');
 const Json2csvParser = require('json2csv').Parser;
-const times = new Date();
-const fields = ['title', 'price', 'imageURL', 'url', 'time']
-const json2csvParser = new Json2csvParser({fields})
+
+//create fields for list items
+const fields = ['Title', 'Price', 'ImageURL', 'URL', 'Time'];
+const json2csvParser = new Json2csvParser({ fields });
 const listItems = [{}];
-//const options = {}
+
+//Print Error Messages
+const errorMessage = (error) => {
+  console.error(`Error: ${error.message}`);
+};
+
 //check to see if a folder named 'data' exist
 fs.stat('data', function (error, stats) {
   //if it does not create the data directory
@@ -15,45 +22,57 @@ fs.stat('data', function (error, stats) {
     fs.mkdir('data', function (error) {
       if (error) throw error;
       console.log('saved');
+    });//end of fs.mkdir
+  }//end of 'if'
+});//end of fs.stat
 
-    });
-  }
-});
-let shirtID = [101,102,103,104,105,106,107,108]
-//vist the website http://shirts4mike.com/shirts.php
-// Promise interface
-for (let i = 0; i < shirtID.length; i++){
-scrapeIt(`http://shirts4mike.com/shirt.php?id=${shirtID[i]}`, {
-  title:'.breadcrumb ',
+//create array of shirt page links on website
+let shirtID = [101, 102, 103, 104, 105, 106, 107, 108];
 
-  price: '.price',
+//vist the website http://shirts4mike.com/shirts.php and loop through each shirt page;
+//use scraper to get field values
+for (let i = 0; i < shirtID.length; i++) {
+  //check for protocol error
+  try {
+    scrapeIt(`http://shirts4mike.com/shirt.php?id=${shirtID[i]}`, {
 
-  imageURL: {
-    selector: 'img ',
-    attr: 'src'
-  },
+      Title: '.breadcrumb ',
 
-  url: {
-    selector: '.breadcrumb a',
-    attr: 'href'
-  },
-})
-.then(({ data, response })=> {
-  data.title = data.title.slice(8);
-  return data;
-})
-.then((response) => {
-  console.log(response)
-listItems.push(response);
-listItems.push(times)
-const csv = json2csvParser.parse(listItems);
-let date = new Date().toISOString().slice(0,10);
-    fs.writeFile(date + ".csv", csv, function(error){
-      // console.log(csv);
+      Price: '.price',
+    })
 
-    }
-    );//end of fs
-}).catch(function(error) {
-  console.log(error)
-})//end of catch
+  //create promise to get time and format the title; return the data
+  .then(({ data })=> {
+      data.URL = `http://shirts4mike.com`;
+      data.ImageURL = `${data.URL}/shirt.php?id=${shirtID[i]}`;
+      data.Time = new Date().toLocaleTimeString();
+      data.Title = data.Title.slice(8);
+      return data;
+    })//end of then
+
+  //create promise to push the response data to listItems
+  .then((data) => {
+      console.log(data);
+      listItems.push(data);
+
+      //pass lisitems to the json2csv parser and store in csv variable
+      const csv = json2csvParser.parse(listItems);
+
+      //create and format date variable for file name
+      let date = new Date().toISOString().slice(0, 10);
+
+      //write file to data folder using listitems as data
+      fs.writeFile(`data/${date}.csv`, csv, function (error) {
+      });//end of fs
+
+    })//end of then
+
+    .catch(function (error) {
+      console.error(`There's been a 404 error. Cannot connect to '${error.message.slice(37, 51)}'. Please try 'http://shirts4mike.com'.`);
+    });//end of catch
+
+    //catch protocol error
+  } catch (error) {
+    errorMessage(error);
+  }//end of catch
 }//end of for loop
